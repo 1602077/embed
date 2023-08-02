208dd9469e87b02cc1488b880d61523e61f9f353
jack <62025739+1602077@users.noreply.github.com>
2023-08-02 23:49:39 +0100

    build(ci): docker push test

--- scripts/setup-gcr-connection.sh
+++ scripts/setup-gcr-connection.sh
@@ -7,7 +7,8 @@ set -eou pipefail
 PROJECT_ID=${PROJECT_ID}
 GITHUB_REPO=${GITHUB_REPO} # username/repo
 SERVICE_ACCOUNT=${SERVICE_ACCOUNT:="github-actions"}
-WORKLOAD_IDENTITY_POOL=${WORKLOAD_IDENTITY_POOL:=github-actions}
+WORKLOAD_IDENTITY_POOL=${WORKLOAD_IDENTITY_POOL:="github-actions"}
+WORKLOAD_PROVIDER=${WORKLOAD_PROVIDER:="github-actions-provider"}
 
 # create service account and assign artifact registry writer permissions.
 gcloud iam service-accounts create "${SERVICE_ACCOUNT}" \
@@ -19,7 +20,8 @@ gcloud projects add-iam-policy-binding $PROJECT_ID \
 
 gcloud services enable iamcredentials.googleapis.com --project "${PROJECT_ID}"
 
-# create workload identity pool & bind service account to it.
+ create workload identity pool &
+bind service account to it.
 gcloud iam workload-identity-pools create "${WORKLOAD_IDENTITY_POOL}" \
 	--project="${PROJECT_ID}" \
 	--location="global" \
@@ -30,7 +32,6 @@ WORKLOAD_IDENTITY_POOL_ID=$(
 		--project="${PROJECT_ID}" \
 		--location="global" \
 		--format="value(name)"
-	projects/750576968/locations/global/workloadIdentityPools/github-actions
 )
 
 gcloud iam workload-identity-pools providers create-oidc "${WORKLOAD_PROVIDER}" \
@@ -46,7 +47,7 @@ gcloud iam service-accounts add-iam-policy-binding "${SERVICE_ACCOUNT}@${PROJECT
 	--role="roles/iam.workloadIdentityUser" \
 	--member="principalSet://iam.googleapis.com/${WORKLOAD_IDENTITY_POOL_ID}/attribute.repository/${GITHUB_REPO}"
 
-WORKLOAD_IDENITY_PROVIDER=$(
+WORKLOAD_IDENITY_PROVIDER_ID=$(
 	gcloud iam workload-identity-pools providers describe "${WORKLOAD_PROVIDER}" \
 		--project="${PROJECT_ID}" \
 		--location="global" \
@@ -54,4 +55,4 @@ WORKLOAD_IDENITY_PROVIDER=$(
 		--format="value(name)"
 )
 
-echo -e ">> pool created with provider '${WORKLOAD_IDENITY_PROVIDER}'"
+echo -e ">> pool created with provider '${WORKLOAD_IDENITY_PROVIDER_ID}'"
