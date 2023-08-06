8ea2b9b6634b80ce517ce594144964225f45dbb9
jack <62025739+1602077@users.noreply.github.com>
2023-08-06 22:28:15 +0100

    deploy: bash script to install knative

---
+++ deploy/scripts/install-knative.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+# install knative via yaml operators.
+# knative does not currently support helm installation.
+cd "${0%/*}"
+
+. ./common/check-env.sh
+. ./common/error.sh
+. ./common/print.sh
+
+KNATIVE_VERSION=${KNATIVE_VERSION:=1.10.1}
+
+print-header "installing knative version:$KNATIVE_VERSION"
+
+check-env
+
+kubectl apply -f "https://github.com/knative/serving/releases/download/knative-v${KNATIVE_VERSION}/serving-crds.yaml"
+kubectl apply -f "https://github.com/knative/serving/releases/download/knative-v${KNATIVE_VERSION}/serving-core.yaml"
+kubectl apply -f "https://github.com/knative/net-istio/releases/download/knative-v${KNATIVE_VERSION}/net-istio.yaml"
